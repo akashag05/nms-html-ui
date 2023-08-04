53ca32924a229105cb76d274b94077ffe5c504ce
akashag05 <124151772+akashag05@users.noreply.github.com>
2023-08-04 11:06:11 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const toggleButton = document.getElementById("toggleButton");
+const gridView = document.querySelector(".grid-view");
+const tableView = document.querySelector(".table-view");
+
+toggleButton.addEventListener("click", () => {
+  if (gridView.style.display === "none") {
+    gridView.style.display = "block";
+    tableView.style.display = "none";
+  } else {
+    gridView.style.display = "none";
+    tableView.style.display = "block";
+  }
+});
+
+const openModalBtn = document.getElementById("openModalBtn");
+const myModal = document.getElementById("myModal");
+const saveBtn = document.getElementById("saveBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const closeCrossBtn = document.getElementById("closeCrossBtn");
+
+openModalBtn.addEventListener("click", (event) => {
+  event.preventDefault(); // Prevent the default link behavior
+  myModal.style.display = "block";
+});
+
+closeModalBtn.addEventListener("click", () => {
+  myModal.style.display = "none";
+});
+
+closeCrossBtn.addEventListener("click", () => {
+  myModal.style.display = "none";
+});
+
+saveBtn.addEventListener("click", () => {
+  // Perform any actions you want to do when the Save button is clicked
+  myModal.style.display = "none"; // Close the modal after saving
+});
+
+$(function () {
+  $("ul.left-menu li > ul").hide();
+  $("ul.left-menu li").click(function (e) {
+    e.stopPropagation();
+    $(this).children("ul").slideToggle();
+  });
+  $("ul.left-menu a").click(function (e) {
+    //e.stopPropagation();
+  });
+});
